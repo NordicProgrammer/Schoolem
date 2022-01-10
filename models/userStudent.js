9162bbdea51758580858bb6c021bb418c024ab8a
NordicProgrammer <olvrbrock@gmail.com>
2022-01-09 20:17:53 -0800

    dev0.0.5

--- models/userStudent.js
+++ models/userStudent.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 const encryptPassword = require('../functions/encryptPassword')
-mongoose.connect('mongodb://192.168.0.47:27017/Schoolem');
+mongoose.connect('mongodb://127.0.0.1:27017/Schoolem');
 
 const { 
     v1: uuidv1,
     v4: uuidv4,
   } = require('uuid');
+const e = require('express');
 
 const studentSchema = new mongoose.Schema({
     firstName: String,
@@ -15,16 +16,24 @@ const studentSchema = new mongoose.Schema({
     uuid: String,
     friends: [{uuid: String}]
 })
-studentSchema.methods.signUp = function(firstName, lastName, email, password){
+studentSchema.methods.signUp = function(firstName, lastName, email, password, cb){
     this.firstName = firstName
     this.lastName = lastName
     this.email = email
     this.password = password
+    mongoose.model('studentUser').find({email: email}, function(err, doc){
+        if(err){
+            cb(false, 0)
+        }else{
+            
+        }
+        
+    })
+    
 }
 studentSchema.methods.test = function(){
     console.log(this.email)
 }
 const studentUser = mongoose.model('studentUser', studentSchema);
 var student = new studentUser({})
-student.signUp('Oliver', 'Brock', 'suzanne_brock@hotmail.com', 'T!lly2019!')
-student.test()
+
